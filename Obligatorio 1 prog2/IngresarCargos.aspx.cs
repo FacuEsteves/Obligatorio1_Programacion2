dcfde094bfcae0d4a9ceaf7cfea5d8d61b2da648
facun <facun@DESKTOP-RVP0945>
2022-10-12 17:52:30 -0300

    se agrego pantalla de ingreso de cargos

--- Obligatorio 1 prog2/IngresarCargos.aspx.cs
+++ Obligatorio 1 prog2/IngresarCargos.aspx.cs
@@ -11,6 +11,7 @@ namespace Obligatorio_1_prog2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            GridCargos.DataSource = Global.transitoMaritimo.cargos;
             GridCargos.DataBind();
         }
 
