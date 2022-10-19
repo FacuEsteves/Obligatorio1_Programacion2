7ac68fb2175b59916ba4e528a61ba45d83aefec1
MateoGutierre <108164216+MateoGutierre@users.noreply.github.com>
2022-10-18 22:55:33 -0300

    asignar tripulantes (falta verificar cantidad y cargo)

--- Obligatorio 1 prog2/Asignar Tripulacion.aspx.cs
+++ Obligatorio 1 prog2/Asignar Tripulacion.aspx.cs
@@ -11,13 +11,18 @@ namespace Obligatorio_1_prog2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            DD_Barcos.DataSource = Persistencia.ListaBarcos();
-            DD_Barcos.DataTextField = "nombre";
-            DD_Barcos.DataValueField = "nombre";
-            DD_Barcos.DataBind();
-
+            if (!IsPostBack)
+            {
+                DD_Barcos.DataSource = Persistencia.ListaBarcos();
+                DD_Barcos.DataTextField = "nombre";
+                DD_Barcos.DataValueField = "nombre";
+                DD_Barcos.DataBind();
+            }
             GridAsignar.DataSource = Persistencia.TripulantesSinAsignar();
             GridAsignar.DataBind();
+
+            GridAsignados.DataSource = Persistencia.TripulantesAsignados(DD_Barcos.SelectedValue);
+            GridAsignados.DataBind();
         }
 
         protected void GridAsignar_SelectedIndexChanged(object sender, EventArgs e)
@@ -53,13 +58,60 @@ namespace Obligatorio_1_prog2
             GridAsignar.DataSource = Persistencia.TripulantesSinAsignar();
             GridAsignar.DataBind();
 
-            //llamar al otro grid para actualizar
+            Persistencia.guardarDatos();
+
+            GridAsignados.DataSource = Persistencia.TripulantesAsignados(DD_Barcos.SelectedValue);
+            GridAsignados.DataBind();
+
+
+
         }
 
         protected void GridAsignados_SelectedIndexChanged(object sender, EventArgs e)
         {
+            
 
         }
 
+        protected void DD_Barcos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridAsignados.DataSource = Persistencia.TripulantesAsignados(DD_Barcos.SelectedValue);
+            GridAsignados.DataBind();
+
+            GridAsignar.DataSource = Persistencia.TripulantesSinAsignar();
+            GridAsignar.DataBind();
+        }
+
+        protected void GridAsignados_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            String cedula = e.Values["cedula"].ToString();
+
+            LabelError.Text = "";
+
+            if (DD_Barcos.SelectedIndex == -1)
+            {
+                LabelError.Text = "Elija un barco";
+                return;
+            }
+            String barcoAsignados = DD_Barcos.SelectedValue;
+
+            for (int i = 0; i < Global.transitoMaritimo.tripulantes.Count; i++)
+            {
+                if (Convert.ToUInt32(cedula) == Global.transitoMaritimo.tripulantes[i].cedula)
+                {
+                    Global.transitoMaritimo.tripulantes[i].NombreBarco = null;
+                    LabelError.Text = "Se elimino el tripulante " + cedula + " del barco " + barcoAsignados;
+                    break;
+                }
+            }
+
+            GridAsignados.DataSource = Persistencia.TripulantesAsignados(DD_Barcos.SelectedValue);
+            GridAsignados.DataBind();
+
+            GridAsignar.DataSource = Persistencia.TripulantesSinAsignar();
+            GridAsignar.DataBind();
+
+            Persistencia.guardarDatos();
+        }
     }
 }
