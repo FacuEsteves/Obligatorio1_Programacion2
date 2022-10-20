bdb1e20173e518e133461da9071521e87d2cbfa0
FedericoBarboza <21fede95@gmail.com>
2022-10-20 12:30:25 -0300

    Buscar Mantenimiento

--- Obligatorio 1 prog2/BuscarMantenimiento.aspx.cs
+++ Obligatorio 1 prog2/BuscarMantenimiento.aspx.cs
@@ -7,26 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace Obligatorio_1_prog2
 {
-    public partial class BuscarMantenimiento : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            DDBarco.DataSource = Persistencia.ListaBarcos();
-            DDBarco.DataTextField = "nombre";
-            DDBarco.DataValueField = "nombre";
-            DDBarco.DataBind();
-        }
+	public partial class BuscarMantenimiento : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
 
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            String barco = DDBarco.SelectedValue;
-            for (int i = 0; i < Global.transitoMaritimo.barcoLentos.Count; i++)
-            {
-                if (Global.transitoMaritimo.barcoLentos[i].nombre == barco)
-                {
-                    
-                }
-            }
-        }
-    }
+		}
+	}
 }
